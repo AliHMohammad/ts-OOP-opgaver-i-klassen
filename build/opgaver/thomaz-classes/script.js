17e1d17ab416166c435376f0ae0a1edca472f856
AliHMohammad <alihmohammad@hotmail.com>
2023-10-05 14:15:59 +0200

    thomaz torsdag

---
+++ build/opgaver/thomaz-classes/script.js
@@ -0,0 +1,40 @@
+"use strict";
+class Circle {
+    radius;
+    constructor(radius) {
+        this.radius = radius;
+        this.radius = radius;
+    }
+    area() {
+        return Math.PI * this.radius * this.radius;
+    }
+}
+const newCircle = new Circle(4);
+class CircleStatic {
+    radius;
+    constructor(radius) {
+        this.radius = radius;
+        this.radius = radius;
+    }
+    static parse(str) {
+        const radius = JSON.parse(str).radius;
+        return new CircleStatic(radius);
+    }
+}
+const newcircleStatic = CircleStatic.parse('{"radius": 1}');
+const _radiusSymbol = Symbol();
+const _drawSymbol = Symbol();
+class Circle4 {
+    _radius;
+    constructor(_radius) {
+        this._radius = _radius;
+        this[_radiusSymbol] = _radius;
+    }
+    [_drawSymbol]() {
+        console.log("Draw");
+    }
+    get radius() {
+        return this[_radiusSymbol];
+    }
+}
+const newCircle4 = new Circle4(4);
---
+++ src/opgaver/thomaz-classes/script.ts
@@ -0,0 +1,48 @@
+
+
+class Circle {
+
+    constructor(public radius: number) {
+        this.radius = radius
+    }
+
+    area() {
+        return Math.PI * this.radius * this.radius;
+    }
+}
+
+const newCircle = new Circle(4);
+
+
+class CircleStatic {
+    constructor(public radius: number) {
+        this.radius = radius;
+    }
+
+    static parse(str: string) {
+        const radius = JSON.parse(str).radius;
+        return new CircleStatic(radius);
+    }
+}
+
+const newcircleStatic = CircleStatic.parse('{"radius": 1}');
+
+const _radiusSymbol = Symbol();
+const _drawSymbol = Symbol();
+
+class Circle4 {
+    constructor(public _radius: number) {
+        this[_radiusSymbol] = _radius;
+    }
+
+    [_drawSymbol]() {
+        console.log("Draw");
+        
+    }
+
+    get radius() {
+        return this[_radiusSymbol]
+    }
+}
+
+const newCircle4 = new Circle4(4)
